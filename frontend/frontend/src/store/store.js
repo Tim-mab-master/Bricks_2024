2c6e3afc2add3c0e03f59ae61b63932a6f729208
kerwinTheCoder <54coderkwjui@gmail.com>
2024-07-14 00:25:07 +0800

    7/14現在ㄉ東西應該都可以用

--- frontend/frontend/src/store/store.js
+++ frontend/frontend/src/store/store.js
@@ -8,7 +8,7 @@ export default createStore({
     testNum: 0,
     projectName: "專案名稱",
     meetingName: "未命名會議紀錄",
-    auth: "設定authorization",
+    auth: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2VtYWlsIjoiODE4MTV0aW1AZ21haWwuY29tIiwiZXhwIjoxNzIzNDc5NDUzfQ.AZ6q7gSJVI2vt16zHoitKCmf5ED9aORxVLSeDF6QZzU",
   },
   // actions: {
   //   setAuth(authorization) {
