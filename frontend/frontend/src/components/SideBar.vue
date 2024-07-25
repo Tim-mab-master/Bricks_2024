d71b042401b652df0a89514b686fa3cc372f576c
kerwinTheCoder <54coderkwjui@gmail.com>
2024-07-25 16:24:47 +0800

    725_16:24調整修改專案類型、調整會議記錄side_bar_css

--- frontend/frontend/src/components/SideBar.vue
+++ frontend/frontend/src/components/SideBar.vue
@@ -1,78 +1,85 @@
 <template>
   <div id="sidebar">
     <div id="container">
-    <div id="title" @click="menu_clicked">
-      <el-icon id="logo" :color = "incolor"><elementPlus /></el-icon>
-      <p id="title-name">{{ project_name }}</p>
-    </div>
-    <div id="btnBlock">
-      <el-button id="addBtn" @click="addClicked">新增</el-button>
-    </div>
-    
-    <!-- 選單部分 -->
-    <div id="menuBar">  
-      <el-row class="tac">
-      <el-col :span="24">
-        <!-- :active-color = incolor -->
-      <el-menu
-        unique-opened = true
-        class="el-menu-vertical-demo"
-        :active-text-color = incolor
-        @select="selectedItem"
-        @open="handleOpen"
-        @close="handleClose"
-        :active-bg-color= active_color
-        :default-active="activeIndex"
-      >
-        <el-sub-menu index="1" class="menu">
-          <template #title>
-            <el-icon><list /></el-icon>
-            <span>會議記錄</span>
-          </template>
-            <el-menu-item index="1-1" ><router-link to="/all/cards" class="menuText">全部</router-link></el-menu-item>
-            <el-menu-item index="1-2" ><router-link to="/all/trashBox" class="menuText">垃圾桶</router-link></el-menu-item>
-        </el-sub-menu>
-        <el-sub-menu index="2" class="menu">
-        <template #title>
-            <el-icon><opportunity /></el-icon>
-            <span>提案區</span>
-          </template>
-            <el-menu-item index="2-1">全部</el-menu-item>
-            <el-menu-item index="2-2">垃圾桶</el-menu-item>        
-        </el-sub-menu>
-        <el-sub-menu index="3" class="menu">
-          <template #title>
-            <el-icon><span class="material-icons">&#xE4FD;</span></el-icon>
-            <span>組織架構圖</span>
-          </template>
-            <el-menu-item index="3-1">全部</el-menu-item>
-            <el-menu-item index="3-2">垃圾桶</el-menu-item>
-        </el-sub-menu>
-      </el-menu>
-    </el-col>
-  </el-row>
+      <div id="title" @click="menu_clicked">
+        <el-icon id="logo" :color="incolor"><elementPlus /></el-icon>
+        <p id="title-name">{{ project_name }}</p>
+      </div>
+      <div id="btnBlock">
+        <el-button id="addBtn" @click="addClicked">新增</el-button>
+      </div>
 
-  </div> 
-  </div> 
-</div>
-</template> 
+      <!-- 選單部分 -->
+      <div id="menuBar">
+        <el-row class="tac">
+          <el-col :span="24">
+            <!-- :active-color = incolor -->
+            <el-menu
+              unique-opened="true"
+              class="el-menu-vertical-demo"
+              :active-text-color="incolor"
+              @select="selectedItem"
+              @open="handleOpen"
+              @close="handleClose"
+              :active-bg-color="active_color"
+              :default-active="activeIndex"
+            >
+              <el-sub-menu index="1" class="menu">
+                <template #title>
+                  <el-icon><list /></el-icon>
+                  <span>會議紀錄</span>
+                </template>
+                <el-menu-item index="1-1"
+                  ><router-link to="/all/cards" class="menuText"
+                    >全部</router-link
+                  ></el-menu-item
+                >
+                <el-menu-item index="1-2"
+                  ><router-link to="/all/trashBox" class="menuText"
+                    >垃圾桶</router-link
+                  ></el-menu-item
+                >
+              </el-sub-menu>
+              <el-sub-menu index="2" class="menu">
+                <template #title>
+                  <el-icon><opportunity /></el-icon>
+                  <span>提案區</span>
+                </template>
+                <el-menu-item index="2-1">全部</el-menu-item>
+                <el-menu-item index="2-2">垃圾桶</el-menu-item>
+              </el-sub-menu>
+              <el-sub-menu index="3" class="menu">
+                <template #title>
+                  <el-icon
+                    ><span class="material-icons">&#xE4FD;</span></el-icon
+                  >
+                  <span>組織架構圖</span>
+                </template>
+                <el-menu-item index="3-1">全部</el-menu-item>
+                <el-menu-item index="3-2">垃圾桶</el-menu-item>
+              </el-sub-menu>
+            </el-menu>
+          </el-col>
+        </el-row>
+      </div>
+    </div>
+  </div>
+</template>
 
 <script>
-import { ref } from 'vue';
-import { useRouter } from 'vue-router';
-import axios from 'axios';
-import { useStore } from 'vuex';
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+import axios from "axios";
+import { useStore } from "vuex";
 
 export default {
-  components: {
-    
-  },
-  props:{
+  components: {},
+  props: {
     activeIndex: String,
   },
 
-  setup(props,{emit}) {
-    const active_color = ref("#fff")
+  setup(props, { emit }) {
+    const active_color = ref("#fff");
     const project_name = "專案名稱";
     const incolor = "#C91F2F";
     const activeOption = ref(null);
@@ -82,52 +89,49 @@ export default {
     const activeIndex = ref(props.activeIndex);
     const menuClass = ref("menu");
     const store = useStore();
-    
 
     // 按下後字體改變顏色
-    const menu_clicked = () =>{
+    const menu_clicked = () => {
       active_color.value = "#FAE4E7";
     };
 
     // 提供父元件選擇的改變
-    const selectedItem = (index) =>{
+    const selectedItem = (index) => {
       activeOption.value = index;
-      emit('update',index);
+      emit("update", index);
     };
 
     // 提供父元件點擊事件
-    const addClicked = (value) =>{
+    const addClicked = (value) => {
       const date = new Date();
 
       // 獲取年份、月份和日期
       const year = date.getFullYear();
-      const month = (date.getMonth() + 1).toString().padStart(2, '0'); // 月份是從0開始的，所以需要加1
-      const day = date.getDate().toString().padStart(2, '0'); // 確保日期是兩位數
+      const month = (date.getMonth() + 1).toString().padStart(2, "0"); // 月份是從0開始的，所以需要加1
+      const day = date.getDate().toString().padStart(2, "0"); // 確保日期是兩位數
 
       // 格式化為 YYYY-MM-DD
       const formattedDate = `${year}-${month}-${day}`;
 
       const body = {
         // "user_id": "0",
-        "project_id": store.state.records.projectID,
-        "record_name": "",
-        "record_date": formattedDate,
-        "record_department": "",
-        "record_attendances": 0,
-        "record_host_name": "",
-        "record_place": ""
-      }
+        project_id: store.state.records.projectID,
+        record_name: "",
+        record_date: formattedDate,
+        record_department: "",
+        record_attendances: 0,
+        record_host_name: "",
+        record_place: "",
+      };
       axios.post("http://34.81.219.139:5000/add_record", body);
       value = true;
       // emit('showAdd',value);
-      router.push({name: 'newRecord'});
+      router.push({ name: "newRecord" });
     };
 
-    const selected = () =>{
+    const selected = () => {
       menuClass.value = "selected";
     };
-
-
 
     return {
       project_name,
@@ -144,129 +148,123 @@ export default {
       activeIndex,
       selected,
     };
-  }
-}
+  },
+};
 </script>
 
-
-
 <style scoped>
-  #sidebar{
-    z-index: 100;
-    display: flex;
-    width: 200px;
-    height: 100vh;
-    flex-direction: column;
-    justify-content: center;
-    /* align-items: center; */
-    vertical-align: top;
-    flex-shrink: 0;
-    position: fixed;
-    /* top: 0;
+#sidebar {
+  z-index: 100;
+  display: flex;
+  width: 200px;
+  height: 100vh;
+  flex-direction: column;
+  /* justify-content: center; */
+  /* align-items: center; */
+  vertical-align: top;
+  flex-shrink: 0;
+  position: fixed;
+  /* top: 0;
     left: 0; */
-    border-right: 1px solid  #E4E7ED;
-    background: #FFF;
-    box-shadow: 0px 0px 12px 0px rgba(0, 0, 0, 0.12);
-  }
-  #container{
-    position: inherit;
-    top: 0;
-  }
+  border-right: 1px solid #e4e7ed;
+  background: #fff;
+  box-shadow: 0px 0px 12px 0px rgba(0, 0, 0, 0.12);
+}
+#container {
+  width: 100%;
+}
 
-  #title{
-    display: flex;
-    width: 100%;
-    padding: 0px 16px;
-    justify-content: left;
-    align-items: center;
-    height: 48px;
-    position: relative;
-    top: 0;
-    left: 0px;
-    /* padding-left: 16px; */
-  }
+#title {
+  display: flex;
+  width: 100%;
+  padding: 0px 16px;
+  justify-content: left;
+  align-items: center;
+  height: 48px;
+  position: relative;
+  top: 0;
+  left: 0px;
+  /* padding-left: 16px; */
+}
 
-  
+#title #logo {
+  display: block;
+  width: auto;
+  height: auto;
+  padding: 4px;
+}
 
-  #title #logo{
-    display: block;
-    width: auto;
-    height: auto;
-    padding: 4px;
-  }
-
-  #title #title-name{
-    color: #303133;
+#title #title-name {
+  color: #303133;
   /* CN bold/Headline3-Bold */
-    font-size: 16px;
-    font-style: normal;
-    font-weight: 600;
-    line-height: 24px; /* 150% */
-    z-index: 10;
-  }
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 24px; /* 150% */
+  z-index: 10;
+}
 
-  #addBtn{
-    display: flex;
-    width: 99%;
-    min-height: 24px;
-    max-height: 40px;
-    padding: 20px 16px;
-    justify-content: center;
-    align-items: center;
-    gap: var(--space-size-small, 8px);
-    flex: 1 0 0;
+#addBtn {
+  display: flex;
+  width: 99%;
+  min-height: 24px;
+  max-height: 40px;
+  padding: 20px 16px;
+  justify-content: center;
+  align-items: center;
+  gap: var(--space-size-small, 8px);
+  flex: 1 0 0;
 
-    border-radius: 999px;
-    color:#FFF;
-    background: #C91F2F;
-    position: relative;
-    top: 0;
-    
-    font-size: 14px;
-    font-style: normal;
-    font-weight: 400;
-    line-height: 32px;
-  }
+  border-radius: 999px;
+  color: #fff;
+  background: #c91f2f;
+  position: relative;
+  top: 0;
 
-  #btnBlock{
-    padding: 12px;
-    position: relative;
-    top:0;
-  }
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 32px;
+}
 
-  .el-menu-vertical{
+#btnBlock {
+  padding: 12px;
+  position: relative;
+  top: 0;
+}
 
-  }
+.el-menu-vertical {
+}
 
-  #menuBar{
-    font-size: 14px;
-    font-style: normal;
-    font-weight: 400;
-    line-height: 24px;
-  }
+#menuBar {
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 24px;
+  widows: 100%;
+  /* border: 2px solid black; */
+}
 
-  /* #menuBar .menu{
+/* #menuBar .menu{
     color: #C91F2F;
   } */
 
+.menu :hover {
+  background-color: #fae4e7;
+  color: #c91f2f;
+}
 
-  .menu :hover{
-    background-color: #FAE4E7;
-    color: #C91F2F;
-  }
+.link {
+  text-decoration: none;
+}
 
-  .link{
-    text-decoration: none;
-  }
+.selected {
+  background-color: #fae4e7;
+  color: #c91f2f;
+}
 
-  .selected{
-    background-color: #FAE4E7;
-    color:#C91F2F;
-  }
-  
-  .menuText{
-    text-decoration: none;
-    color: black;
-  }
-
+.menuText {
+  text-decoration: none;
+  color: black;
+}
 </style>
