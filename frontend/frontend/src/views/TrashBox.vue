9a01d1331815405e399e1c320d0b076f040c5e97
kerwinTheCoder <54coderkwjui@gmail.com>
2024-08-20 22:13:23 +0800

    正在做永久刪除專案彈出視窗

--- frontend/frontend/src/views/TrashBox.vue
+++ frontend/frontend/src/views/TrashBox.vue
@@ -2,7 +2,64 @@
   <div>
     <side-bar></side-bar>
     <nav-bar-all class="navBar"></nav-bar-all>
-
+    <div class="terminate_confirm" v-if="close_terminate">
+      <div
+        class="close_terminate_delete_confirm"
+        @click="close_terminate_confirm"
+      ></div>
+      <h4 class="confirm_title">結束專案</h4>
+      <p class="confirm_content">
+        確認結束此專案？執行後請至「已結束專案」查看
+      </p>
+      <div class="confirm_button_container">
+        <div class="confirm_button cb_cancle" @click="close_terminate_confirm">
+          取消
+        </div>
+        <div
+          class="confirm_button cb_confirm"
+          @click="confirm_terminate_button"
+        >
+          確定
+        </div>
+      </div>
+    </div>
+    <div class="delete_confirm" v-if="close_delete">
+      <div
+        class="close_terminate_delete_confirm"
+        @click="close_delete_confirm"
+      ></div>
+      <h4 class="confirm_title">刪除專案</h4>
+      <p class="confirm_content">確認刪除此專案？執行後請至「垃圾桶」查看</p>
+      <div class="confirm_button_container">
+        <div class="confirm_button cb_cancle" @click="close_delete_confirm">
+          取消
+        </div>
+        <div class="confirm_button cb_confirm" @click="confirm_delete_button">
+          確定
+        </div>
+      </div>
+    </div>
+    <div class="forever_delete_confirm" v-if="forever_delete_true">
+      <div
+        class="close_forever_delete_confirm"
+        @click="close_forever_delete"
+      ></div>
+      <h4 class="forever_delete_title">永久刪除會議記錄</h4>
+      <p class="forever_delete_content">
+        確認結束此會議記錄？請注意，永久刪除後將無法復原
+      </p>
+      <div class="confirm_button_container">
+        <div class="confirm_button cb_cancle" @click="close_terminate_confirm">
+          取消
+        </div>
+        <div
+          class="confirm_button cb_confirm"
+          @click="confirm_terminate_button"
+        >
+          永久刪除
+        </div>
+      </div>
+    </div>
     <div class="navAndCont" id="trash">
       <trash-bar class="navBar"></trash-bar>
       <div class="cards">
@@ -41,10 +98,13 @@ export default {
       store.dispatch("fetchTrashRecords");
       console.log("allRecordsTrash", store.getters.getTrashRecords);
     });
-
+    const forever_delete_true = ref(false);
+    const close_forever_delete = () => {
+      forever_delete_true.value = !forever_delete_true.value;
+    };
     const cards = computed(() => store.getters.getTrashRecords);
     // const cards = store.getters.getTrashRecords;
-    return { cards };
+    return { cards, forever_delete_true, close_forever_delete };
   },
 };
 </script>
@@ -72,6 +132,107 @@ export default {
   margin: 28px 1.5vw 28px 28px;
   /* border: 2px solid black; */
 }
+
+/* 確認刪除 */
+.forever_delete_confirm {
+  width: 380px;
+  height: 160px;
+  position: fixed;
+  border-radius: 14px;
+  background-color: white;
+  box-shadow: 0px 8px 12px rgba(0, 0, 0, 0.4);
+  z-index: 6;
+  left: 36%;
+  top: 35%;
+}
+
+.close_forever_delete_confirm {
+  position: relative;
+  width: 24px;
+  height: 24px;
+  cursor: pointer;
+  top: 10px;
+  left: 307px;
+}
+
+.close_forever_delete_confirm::before,
+.close_forever_delete_confirm::after {
+  content: "";
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  width: 2px;
+  height: 17px;
+  background-color: black;
+}
+.close_forever_delete_confirm::before {
+  transform: translate(-50%, -50%) rotate(45deg);
+}
+
+.close_forever_delete_confirm::after {
+  transform: translate(-50%, -50%) rotate(-45deg);
+}
+
+.forever_delete_title {
+  font-size: 22px;
+  font-weight: 950;
+  position: relative;
+  top: -30px;
+  text-align: center;
+  user-select: none;
+}
+
+.forever_delete_content {
+  font-size: 14px;
+  font-weight: 600;
+  position: relative;
+  top: -44px;
+  text-align: center;
+  user-select: none;
+}
+
+.confirm_button_container {
+  width: 280px;
+  height: 36px;
+  /* border: 2px solid black; */
+  top: -38px;
+  left: 50%;
+  transform: translate(-50%);
+
+  display: flex;
+  position: relative;
+  justify-content: space-around;
+
+  text-align: center;
+  font-size: 16px;
+  font-weight: 500;
+  line-height: 40px;
+  letter-spacing: 1.25px;
+}
+
+.confirm_button {
+  width: 120px;
+  height: 38px;
+  border-radius: 12px;
+
+  text-align: center;
+
+  cursor: pointer;
+  user-select: none;
+}
+
+.cb_cancle {
+  background-color: #fff;
+  color: black;
+  font-weight: 950;
+}
+
+.cb_confirm {
+  background-color: #b82c30;
+  color: white;
+  font-weight: 700;
+}
+
 .navAndCont {
   background-color: #dcdfe6;
   position: absolute;
