bf66e0e476f22652ade192b84145585fc6674f66
kerwinTheCoder <54coderkwjui@gmail.com>
2024-09-09 00:37:32 +0800

    9/9 0:37 修復註冊、登入頁面css，包含字體大小，行距, marging等

--- frontend/frontend/src/views/Login.vue
+++ frontend/frontend/src/views/Login.vue
@@ -457,7 +457,7 @@ export default {
 .title {
   width: 100%;
   height: 36px;
-  font-size: 24.75px;
+  font-size: 24px;
   text-align: center;
   font-family: "Noto Sans TC";
   letter-spacing: 0.1875px;
@@ -639,11 +639,11 @@ input::placeholder {
 }
 
 .other_resource div {
-  width: 44%;
+  width: 40%;
   height: 100%;
   border: 1px solid #b6aeae;
   border-radius: 14px;
-  font-size: 13.5px;
+  font-size: 14.5px;
   line-height: 34.5px;
   color: #b6aeae;
   font-weight: 500;
@@ -651,13 +651,13 @@ input::placeholder {
   letter-spacing: 0.9375px;
   float: left;
   position: relative;
-  text-indent: 33px;
+  text-indent: 38px;
   background-color: white;
   cursor: pointer;
 }
 
 #FB_login_btn {
-  width: 146.25px;
+  width: 160px;
   float: right;
 }
 
@@ -779,7 +779,7 @@ input::placeholder {
 
 @media screen and (min-width: 1920px) {
   .middle {
-    width: 432px;
+    width: 576px;
     height: 402px;
     position: absolute;
     top: 108.75px;
@@ -806,7 +806,7 @@ input::placeholder {
 
 @media screen and (min-width: 1600px) {
   .middle {
-    width: 352.5px;
+    width: 470px;
     height: 402px;
     position: absolute;
     top: 108.75px;
@@ -835,7 +835,7 @@ input::placeholder {
 @media screen and (min-width: 768px) and (max-width: 1599px) {
   /* OKay */
   .middle {
-    width: 312px;
+    width: 360px;
     height: 402px;
     position: absolute;
     top: 108.75px;
